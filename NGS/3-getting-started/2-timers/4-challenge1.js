d2937a91e3e7a46229d9374254900c9bd238ec1c
SamShurts <sam.fk.shurts@gmail.com>
2023-04-04 22:27:51 -0700

    Completed section 3 challenges

--- NGS/3-getting-started/2-timers/4-challenge1.js
+++ NGS/3-getting-started/2-timers/4-challenge1.js
@@ -1,6 +1,10 @@
-const theOneFunc = () => {};
+const theOneFunc = msg => {
+    console.log(msg);
+};
 
-setTimeout(theOneFunc, 4 * 1000);
+setTimeout(theOneFunc, 4 * 1000, "Hello after 4 seconds");
+
+setTimeout(theOneFunc, 8 * 1000, "Hello after 8 seconds");
 
 // Hello after 4 seconds
 
--- NGS/3-getting-started/2-timers/8-challenge2.js
+++ NGS/3-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,16 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+let counter = 0;
+const interval_func = setInterval( () => {
+    
+    console.log("Hello World");
+    counter++;
+
+    if (counter === 5) {
+        clearInterval(interval_func);
+        console.log("Done");
+    }
+    
+}, 1000);
