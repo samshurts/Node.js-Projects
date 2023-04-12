2d3285a4a0a0a46846caaf31b94abfdce105812d
SamShurts <sam.fk.shurts@gmail.com>
2023-04-11 18:05:00 -0700

    Installed chalk, debug, morgan and nodemon

--- Express-Web-App/app.js
+++ Express-Web-App/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
-
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.send('Hello from my app');
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000...')
-})
+    debug(`Listening on port ${chalk.green('3000')}`);
+});
