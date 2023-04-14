16d89e7f2c41940d2619c1f2795d7569c24c58b5
SamShurts <sam.fk.shurts@gmail.com>
2023-04-13 21:42:02 -0700

    Completed section 6 of "Building web apps with node.js and express"

--- Express-Web-App/app.js
+++ Express-Web-App/app.js
@@ -6,12 +6,15 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+app.use('/sessions', sessionsRouter);
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
